ae867bb0c7e76f50dff73d771df177b0e8c211e7
Afonso Silva <a16375@cic.pt>
2025-03-24 17:07:21 +0000

    update

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace HorsesEmpire
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            UserAppTheme = AppTheme.Light;
+        }
+    }
+}
