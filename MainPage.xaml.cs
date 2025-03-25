2a399e0c1ae3700bb1477e4e08c3bbbba641e21f
Afonso Silva <a16375@cic.pt>
2025-03-25 23:37:22 +0000

    upgrade store page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,11 +4,19 @@ namespace HorsesEmpire
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        int count = 0; int moneyclick=1; int walletmoney;
 
         public MainPage()
         {
             InitializeComponent();
+
+        }
+        public void Click(object sender, EventArgs e)
+        {
+            count++;
+            walletmoney += moneyclick;
+            money.Text=walletmoney.ToString()+"€";
+
         }
 
        
