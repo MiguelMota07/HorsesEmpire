87b459fa232f5ba94be60477e6a34d47ef960801
MiguelMota07 <MiguelMota07@users.noreply.github.com>
2025-03-26 14:30:36 +0000

    Added ClickMoney feature

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,12 +2,29 @@
 {
     public partial class App : Application
     {
+        public static int Money { get; set; }
+        public static int MoneyPerClick { get; set; }
+        public static int MoneyPerSecond { get; set; }
+        public static int ClickUpgradeCost { get; set; }
+        public static int AllMoney { get; set; }
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
             UserAppTheme = AppTheme.Light;
+
+            InitUserData();
+        }
+
+        private void InitUserData()
+        {
+            Money = 0;
+            MoneyPerClick = 1;
+            MoneyPerSecond = 0;
+            AllMoney = 0;
+            ClickUpgradeCost = 40;
         }
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,15 +4,41 @@ namespace HorsesEmpire
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+
+            moneyperclick.Text = App.MoneyPerClick.ToString() + "€/click";
+            moneypersecond.Text = App.MoneyPerSecond.ToString() + "€/s";
+            upgradeclick.Text = "Upgrade Click " + App.ClickUpgradeCost.ToString() + "€";
         }
 
-       
+       public void OnClickMoneyButton(object sender, EventArgs e)
+       {
+            App.Money += App.MoneyPerClick;
+            App.AllMoney += App.MoneyPerClick;
+            
+            money.Text = App.Money.ToString() + "€";
+        }
 
+       public void OnClickUpgradeButon(object sender, EventArgs e)
+       {
+            float upcost = App.ClickUpgradeCost;
+            int Money = App.Money;
+
+            if (Money > upcost) {
+                App.Money -= (int)upcost;
+                if (App.MoneyPerClick == 1)
+                    App.MoneyPerClick = 2;
+                else
+                    App.MoneyPerClick = (int)(App.MoneyPerClick * 1.5);
+                App.ClickUpgradeCost = (int)(upcost * 2);
+
+                moneyperclick.Text = App.MoneyPerClick.ToString() + "€/click";
+                money.Text = App.Money.ToString() + "€";
+                upgradeclick.Text = App.ClickUpgradeCost.ToString() + "€";
+            }
+        }
     }
 
 }
